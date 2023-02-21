8a3b8edca7a50f69e73a5aa22cc19efc6fbdc773
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-02-21 11:39:33 +0000

    commit1

--- class_person.rb
+++ class_person.rb
@@ -24,3 +24,32 @@ end
 #  other_person.age
 # Output:
 #=>  45
+class Person
+  attr_accessor :first_name
+  attr_accessor :last_name
+  require("date")
+  attr_accessor :birthdate
+  def full_name
+    return self.first_name + " " + self.last_name
+  end
+  
+
+  def age
+    dob = Date.parse(self.birthdate)
+    now = Date.today
+    age_in_days = now - dob
+    age_in_years = age_in_days / 365
+
+    return age_in_years.to_i
+  end
+end
+hs = Person.new
+hs.first_name = "Sunil"
+hs.last_name = "Patel"
+ p hs.full_name
+hs = Person.new
+hs.birthdate = "April 19, 1987"
+
+ p hs.age # => 32, as of this writing
+
+
